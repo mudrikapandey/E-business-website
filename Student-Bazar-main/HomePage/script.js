88464831ffa634b19f6b016128f46aec7c360bd9
Mudrika Pandey <mudrikapandey99@gmail.com>
2024-02-08 20:04:21 +0530

    first commit

---
+++ Student-Bazar-main/HomePage/script.js
@@ -0,0 +1,31 @@
+gsap.from(".shopAndSave h1"  , {
+      y:30,
+      opacity:0,
+      delay:0.5,
+      duration:0.9,
+      stagger:0.4
+})
+
+gsap.from(".left-writ-tex h3" , {
+    y:30,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    stagger:0.4
+})
+
+gsap.from(".studentEssentail h4" , {
+    y:30,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    stagger:0.4
+})
+
+
+ var loader = document.getElementById("preloder");
+
+ window.addEventListener("load",function() {
+       loader.style.display = "none";
+ })
+ 
